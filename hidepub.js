02a26a84825bb1683501b2cc8d6b2ea51619c14e
expertad <63313365+expertad@users.noreply.github.com>
2020-04-07 22:45:24 +0100

    Update hidepub.js

--- hidepub.js
+++ hidepub.js
@@ -1,4 +1,3 @@
 var ad = "aGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmU=";
 var decodedad = atob(ad);
 var func = new Function(decodedad);
-func();
