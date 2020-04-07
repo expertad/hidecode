3acedd29ea3f5262baa40dcf3bef12eb7b889820
expertad <63313365+expertad@users.noreply.github.com>
2020-04-07 19:16:48 +0100

    Create hidepub.js

---
+++ hidepub.js
@@ -0,0 +1,4 @@
+var ad = "aGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmU=";
+var decodedad = atob(ad);
+var func = new Function(decodedad);
+func();
